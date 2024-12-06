941906485e0aa5d67bd8d06c7fe327e9c260851b
MartinNuc <martin.nuc@productboard.com>
2024-12-06 15:16:54 +0100

    reducer example

--- react-course/src/router/welcome.jsx
+++ react-course/src/router/welcome.jsx
@@ -1,3 +1,58 @@
+/**
+ * Zvire
+ * 
+ * - eat
+ * - sleep
+ * - die
+ * - getOlder
+ */
+
+import { useReducer } from "react"
+
+const initialZvire = {
+  hungry: true,
+  age: 0,
+  rested: false,
+  alive: true
+}
+
+function zvireReducer(state, action) {
+  switch (action.type) {
+    case 'eat':
+      if (!state.alive) { return state; }
+      return {
+        ...state,
+        hungry: false
+      }
+    case 'sleep':
+      if (!state.alive) { return state; }
+      return {
+        ...state,
+        rested: true,
+        hungry: true
+      }
+    case 'getOlder':
+      if (state.age > 10) {
+        return {
+          ...state,
+          alive: false
+        }
+      }
+      return {
+        ...state,
+        age: state.age + 1,
+      }
+  }
+}
+
 export const Welcome = () => {
-  return <h1>Welcome to our page!!</h1>
+  const [state, dispatch] = useReducer(zvireReducer, initialZvire);
+
+  return <div>
+    <pre>{JSON.stringify(state, null, 2)}</pre>
+    <button onClick={() => dispatch({type: 'eat'})}>Eat</button>
+    <button onClick={() => dispatch({type: 'sleep'})}>Sleep</button>
+    <button onClick={() => dispatch({type: 'getOlder'})}>Get older</button>
+    <h1>Welcome to our page!!</h1>
+  </div>
 }
